5c4d456fe60237d9510d5e8eb432b030e8c4d43b
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-02-28 13:19:07 -0300

    Complete exercices from 6 to 10

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR"; 
+    if(str === '') return ''; 
+    
+    let result = ""; 
+    for(let i = 0; i < num; i++){
+        result += str; 
+    }
 
+    return result; 
 };
 
 // Do not edit below this line
