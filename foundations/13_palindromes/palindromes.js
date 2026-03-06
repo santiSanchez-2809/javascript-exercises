b48009f58ac0f1015945bc96b56fb7c6ec53299e
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-06 00:04:09 -0300

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const arr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => isLetterOrNumber(char));
+    const reversed = [...arr].reverse(); 
 
+    for(let i = 0; i < arr.length; i++){ 
+        if(reversed[i] !== arr[i]) return false;   
+    }
+
+    return true; 
 };
+
+function isLetterOrNumber(char){
+    return /^[a-z0-9]$/i.test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
