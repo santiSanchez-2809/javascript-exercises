4f4d5ca92b3b07f9220778ff07de83a23f865acf
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-06 11:33:42 -0300

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let result = 1; 
+    let previousResult = 0;
+    let aux;
 
+    num = +num;
+
+    if(num === 0) return 0;
+    if(num < 0) return "OOPS"
+
+    for(let i = 1; i < num; i++){ 
+        aux = result;  
+        result += previousResult; 
+        previousResult = aux;
+    }
+    return result; 
 };
 
 // Do not edit below this line
