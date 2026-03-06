2e51d39b5d9554082e0bbefa9c5cef5432468cb6
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-05 22:33:25 -0300

    Complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b; 
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+    if(exponent === 0) return 1; 
+
+    let result = 1; 
+
+    for(let i = 0; i < exponent; i++){
+      result *= base; 
+    }
+
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let result = 1;
+
+    for(let i = 2; i <= num; i++){
+      result *= i; 
+    }
+
+    return result; 
 };
 
 // Do not edit below this line
