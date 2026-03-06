52f16e1f8d01185374c065ce2a2551e6ac35a5d3
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-06 12:18:05 -0300

    Complete 16_find_the_oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((currentPerson, oldestPerson) => 
+        age(currentPerson) > age(oldestPerson)? currentPerson
+        : oldestPerson
+        , {yearOfBirth: 0, yearOfDeath: 0});
 };
+
+function age(person){
+    let age = "yearOfDeath" in person
+    ? person.yearOfDeath - person.yearOfBirth
+    : new Date().getFullYear() - person.yearOfBirth;
+    
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
